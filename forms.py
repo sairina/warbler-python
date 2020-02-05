3c4bc5bbcd7e148e2f98d80ececf36adec3cceb5
sairina <sairina.merinotsui@gmail.com>
2020-02-05 11:10:32 -0800

    fixed user profile to add bio, location, header img; fixed user cards for followers, following, list-users pages to add bio

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, Optional
 
 
 class MessageForm(FlaskForm):
@@ -15,7 +15,11 @@ class UserAddForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[Length(min=6)])
-    image_url = StringField('(Optional) Image URL')
+    image_url = StringField('(Optional) Image URL', validators=[Optional()])
+    header_image_url = StringField('(Optional) Header Image URL',
+                                   validators=[Optional()])
+    bio = StringField('(Optional) Bio')
+    location = StringField('(Optional) Location')
 
 
 class LoginForm(FlaskForm):
