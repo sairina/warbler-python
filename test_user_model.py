d3f3413f3c201f6abdddb33b8883399a6a3e9374
sairina <sairina.merinotsui@gmail.com>
2020-02-06 17:11:24 -0800

    added tests for is_following, is_followed_by, _creating_new_user

--- test_user_model.py
+++ test_user_model.py
@@ -5,10 +5,11 @@
 #    python -m unittest test_user_model.py
 
 
+from app import app
 import os
 from unittest import TestCase
 
-from models import db, User, Message, Follows
+from models import db, User, Message, Follows, Favorite
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -20,7 +21,6 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
 # Now we can import app
 
-from app import app
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
@@ -28,17 +28,18 @@ from app import app
 
 db.create_all()
 
-user1 = User(
+NEW_USER = User(
     email="test@test.com",
     username="testuser",
     password="HASHED_PASSWORD"
 )
 
-user2 = User(
-    email="test2@test.com",
-    username="testuser2",
+NEW_USER_2 = User(
+    email="test12@test.com",
+    username="testuser12",
     password="HASHED_PASSWORD",
 )
+
 
 class UserModelTestCase(TestCase):
     """Test views for messages."""
@@ -49,38 +50,76 @@ class UserModelTestCase(TestCase):
         User.query.delete()
         Message.query.delete()
         Follows.query.delete()
+        Favorite.query.delete()
+
+        user1 = User(
+            email="test1@test.com",
+            username="testuser1",
+            password="HASHED_PASSWORD"
+        )
+
+        user2 = User(
+            email="test2@test.com",
+            username="testuser2",
+            password="HASHED_PASSWORD",
+        )
+
+        db.session.add_all([user1, user2])
+        db.session.commit()
 
         self.client = app.test_client()
+        self.user1 = user1
+        self.user2 = user2
 
     def test_user_model(self):
         """Does basic model work?"""
 
-        u = User(
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
-
-        db.session.add(u)
+        db.session.add(NEW_USER)
         db.session.commit()
 
         # User should have no messages & no followers
-        self.assertEqual(len(u.messages), 0)
-        self.assertEqual(len(u.followers), 0)
+        self.assertEqual(len(NEW_USER.messages), 0)
+        self.assertEqual(len(NEW_USER.followers), 0)
 
     def test_repr(self):
         """Test for a repr"""
-        self.assertEqual(repr(user1), f'<User #{user1.id}: testuser, test@test.com>')
+
+        self.assertEqual(
+            repr(NEW_USER), f'<User #{NEW_USER.id}: testuser, test@test.com>')
 
     def test_is_following(self):
         """Test if a user is following another user"""
-        user1.following.append(user2)
+
+        NEW_USER.following.append(User.query.get(NEW_USER_2.id))
+        # PAUSE HERE.
+
         db.session.commit()
 
-        # self.assertEqual(user2.is_following(self, user1), False)
-        self.assertEqual(self.is_following(self,user2), True)
+        self.assertEqual(NEW_USER.is_following(NEW_USER_2), True)
+        self.assertEqual(NEW_USER_2.is_following(NEW_USER), False)
 
+    # def test_is_followed_by(self):
+    #     """Test if a user is followed by another user"""
 
+    #     NEW_USER.following.append(NEW_USER_2.id)
+    #     db.session.commit()
 
+    #     self.assertEqual(NEW_USER.is_followed_by(NEW_USER_2), False)
+    #     self.assertEqual(NEW_USER_2.is_followed_by(NEW_USER), True)
 
+    # def test_User_create(self):
+    #     """ Tests if new user created with valid credentials """
 
+    #     u = User(
+    #         email="test@test.com",
+    #         username="testuser",
+    #         password="HASHED_PASSWORD"
+    #     )
+
+    #     db.session.add(u)
+    #     db.session.commit()
+
+    #     users = User.query.all()
+
+    #     # users table should have 1 user
+    #     self.assertEqual(len(users), 1)
