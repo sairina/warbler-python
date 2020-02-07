b6cc545d6431582b1fce4f4e10faff8cb9a3e43a
sairina <sairina.merinotsui@gmail.com>
2020-02-07 13:11:45 -0800

    finished integrityerror test, added rollback in teardown

--- test_user_model.py
+++ test_user_model.py
@@ -9,6 +9,7 @@ import os
 from unittest import TestCase
 import forms
 from models import db, User, Message, Follows
+from sqlalchemy.exc import IntegrityError
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -59,66 +60,63 @@ class UserModelTestCase(TestCase):
         self.user1 = user1
         self.user2 = user2
 
-    # def test_user_model(self):
-    #     """Does basic model work?"""
+    def tearDown(self):
+        """Rolls back the database for set up."""
 
-    #     new_user = User(
-    #         email="test@test.com",
-    #         username="testuser",
-    #         password="HASHED_PASSWORD"
-    #     )
+        db.session.rollback()
 
-    #     db.session.add(new_user)
-    #     db.session.commit()
+    def test_user_model(self):
+        """Does basic model work?"""
 
-    #     # User should have no messages & no followers
-    #     self.assertEqual(len(new_user.messages), 0)
-    #     self.assertEqual(len(new_user.followers), 0)
-
-    # def test_repr(self):
-    #     """Test for a repr"""
-
-    #     self.assertEqual(
-    #         repr(self.user1), f'<User #{self.user1.id}: testuser1, test1@test.com>')
-
-    # def test_is_following(self):
-    #     """Test if a user is following or is being followed by another user"""
-
-    #     self.assertEqual(self.user1.is_following(self.user2), True)
-    #     self.assertEqual(self.user2.is_following(self.user1), False)
-    #     self.assertEqual(self.user1.is_followed_by(self.user2), False)
-    #     self.assertEqual(self.user2.is_followed_by(self.user1), True)
-
-    # def test_User_create(self):
-    #     """ Tests if new user created with valid credentials """
-
-    #     User.query.delete()
-
-    #     u = User(
-    #         email="test@test.com",
-    #         username="testuser",
-    #         password="HASHED_PASSWORD"
-    #     )
-
-    #     db.session.add(u)
-    #     db.session.commit()
-
-    #     users = User.query.all()
-
-    #     self.assertEqual(len(users), 1)
-
-    def test_creating_user_error(self):
-        """ Tests if no new user created with invalid credentials """
-        
         new_user = User(
             email="test@test.com",
-            username="testuser1",
+            username="testuser",
             password="HASHED_PASSWORD"
         )
 
         db.session.add(new_user)
         db.session.commit()
 
-        with self.assertRaises(Exception) as raised:  # top level exception as we want to figure out its exact type
-            forms.validate_on_submit()
-        self.assertEqual(app.IntegrityError, type(raised.exception))  # if it fails, we'll get the correct type to import
+        # User should have no messages & no followers
+        self.assertEqual(len(new_user.messages), 0)
+        self.assertEqual(len(new_user.followers), 0)
+
+    def test_repr(self):
+        """Test for a repr"""
+
+        self.assertEqual(
+            repr(self.user1), f'<User #{self.user1.id}: testuser1, test1@test.com>')
+
+    def test_is_following(self):
+        """Test if a user is following or is being followed by another user"""
+
+        self.assertEqual(self.user1.is_following(self.user2), True)
+        self.assertEqual(self.user2.is_following(self.user1), False)
+        self.assertEqual(self.user1.is_followed_by(self.user2), False)
+        self.assertEqual(self.user2.is_followed_by(self.user1), True)
+
+    def test_User_create(self):
+        """ Tests if new user created with valid credentials """
+
+        User.query.delete()
+
+        u = User(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD"
+        )
+
+        db.session.add(u)
+        db.session.commit()
+
+        users = User.query.all()
+
+        self.assertEqual(len(users), 1)
+
+    def test_creating_user_error(self):
+        """ Tests if no new user created with invalid credentials """
+
+        with self.assertRaises(IntegrityError) as raised:
+            User.signup("testuser1", "user123@user.com", "PASSWORD", None)
+            db.session.commit()
+        self.assertIn('duplicate key value violates unique constraint', str(raised.exception))  
